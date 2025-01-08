3cee8da740d5af8786c35723089d224a7f02162c
In'am Falahuddin <inamfalahuddin06@gmail.com>
2025-01-08 10:39:07 +0700

    tambah tarif ok tamsel

---
+++ env.js
@@ -0,0 +1,16 @@
+const DIR = "tamsel";
+// const FILE_INPUT = "tarif_gigi";
+// const FILE_INPUT = "tarif_lab";
+// const FILE_INPUT = "tarif_lain";
+// const FILE_INPUT = "tarif_oksigen";
+// const FILE_INPUT = "tarif_rad";
+// const FILE_INPUT = "tarif_ranap";
+// const FILE_INPUT = "tarif_rehab";
+// const FILE_INPUT = "tarif_sewa";
+// const FILE_INPUT = "taraif_vk";
+const FILE_INPUT = "taraif_ok";
+const HOST = "http://localhost/rsakranji/";
+// const HOST = "http://100.100.100.67/farnap/";
+const COOKIE = "rsan_session=9k0p8adcrjuhb06nfetgj09g4602mppl";
+
+module.exports = { FILE_INPUT, COOKIE, HOST, DIR };
