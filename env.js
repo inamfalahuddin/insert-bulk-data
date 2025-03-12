2c742555512990be158fea72d1ff33ccbbd4eeb0
In'am Falahuddin <inamfalahuddin06@gmail.com>
2025-03-12 09:23:56 +0700

    menambah ok kranji dan update readme

--- env.js
+++ env.js
@@ -1,4 +1,4 @@
-const DIR = "tamsel";
+const DIR = "kranji";
 // const FILE_INPUT = "tarif_gigi";
 // const FILE_INPUT = "tarif_lab";
 // const FILE_INPUT = "tarif_lain";
@@ -8,7 +8,8 @@ const DIR = "tamsel";
 // const FILE_INPUT = "tarif_rehab";
 // const FILE_INPUT = "tarif_sewa";
 // const FILE_INPUT = "taraif_vk";
-const FILE_INPUT = "taraif_ok";
+// const FILE_INPUT = "taraif_ok";
+const FILE_INPUT = "tarif_ok_bpjs";
 const HOST = "http://localhost/rsakranji/";
 // const HOST = "http://100.100.100.67/farnap/";
 const COOKIE = "rsan_session=9k0p8adcrjuhb06nfetgj09g4602mppl";
